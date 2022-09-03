413015ea1ed2446a15956a9da44451bebafe8a6b
akitouuu <fixserials06@gmail.com>
2022-09-04 02:06:58 +0500

    JSLVL

--- script/script.js
+++ script/script.js
@@ -2,21 +2,74 @@ const num = document.querySelector('.header__timer-extra')
 
 console.log(num.innerHTML);
 
-function logoSec() {
-    
+function logoSec(lvl = 0) {
+    num.innerHTML = lvl;
+    lvl++;
+    let speed = 50
 
-    
+    if (lvl > 40) {
+        speed = 100;
+    } else if (lvl > 60) {
+        speed = 120;
+    } else if (lvl > 80) {
+        speed = 140
+    } else if (lvl > 90) {
+        speed = 160;
+    }
 
-    if(num.innerHTML < 50) {
-        num.innerHTML++
-        setTimeout(() => logoSec(), 50);
-        
-    }else if(num.innerHTML >= 50) {
-        num.innerHTML++
-        setTimeout(() => logoSec(), 150);
-    }else if(num.innerHTML >= 100){
-        num.innerHTML = 100
-    }else {}
+    if (lvl <= 100) {
+        setTimeout(() => {
+            logoSec(lvl)
+        }, speed);
+    }
     
 }
 logoSec()
+
+
+
+
+
+
+
+const product = {
+    plainBurger: {
+        name: 'Гамбургер простой',
+        price: 10000,
+        img: 'images/product2.jpg',
+        amount: 0,
+        kcall: 400,
+        get totalSum() {
+            return this.price * this.amount
+        },
+        get totalKcall() {
+            return this.kcall * this.amount
+        }
+    },
+    freshBurger: {
+        name: 'Гамбургер FRESH',
+        price: 20500,
+        img: 'images/product1.jpg',
+        amount: 0,
+        kcall: 550,
+        get totalSum() {
+            return this.price * this.amount
+        },
+        get totalKcall() {
+            return this.kcall * this.amount
+        }
+    },
+    freshCombo: {
+        name: 'FRESH COMBO',
+        price: 31900,
+        img: 'images/product3.jpg',
+        amount: 0,
+        kcall: 800,
+        get totalSum() {
+            return this.price * this.amount
+        },
+        get totalKcall() {
+            return this.kcall * this.amount
+        }
+    }
+}
